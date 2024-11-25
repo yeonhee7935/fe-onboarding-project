08f479cec382582412a2778fdc48c100298f8e47
yeonhee7935 <yeonhee7935@naver.com>
2024-11-25 20:05:36 +0900

    [feat] 반환 버튼

--- vendingMachine/input.js
+++ vendingMachine/input.js
@@ -1,7 +1,7 @@
 export function initializeInput(state) {
   const moneyInput = document.getElementById("money-input");
   const insertButton = document.getElementById("insert-money");
-
+  const returnButton = document.getElementById("return-money");
   insertButton.addEventListener("click", () => {
     const value = parseInt(moneyInput.value, 10);
 
@@ -13,6 +13,14 @@ export function initializeInput(state) {
       alert("올바른 금액을 입력해주세요.");
     }
 
+    moneyInput.value = ""; // 입력 필드 초기화
+  });
+  returnButton.addEventListener("click", () => {
+    if (state.balance > 0) {
+      state.addLog(`금액 반환: ${state.balance}원`);
+      state.balance = 0;
+      state.updateDisplay(state.balance);
+    }
     moneyInput.value = ""; // 입력 필드 초기화
   });
 }
