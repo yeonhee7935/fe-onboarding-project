883a19d55ee288cb470cef54beefd91d7cb22e65
yeonhee7935 <yeonhee7935@naver.com>
2024-12-04 20:44:22 +0900

    [test] VendingMachine 테스트 - 사전 상태 검증 추가

--- test/models/VendingMachine.test.js
+++ test/models/VendingMachine.test.js
@@ -18,14 +18,16 @@ describe("VendingMachine", () => {
     vendingMachine = new VendingMachine(products);
   });
 
-  describe("addBalance", () => {
-    it("금액을 투입하면 잔액이 증가한다", () => {
+  describe("addBalance()", () => {
+    it("금액을 투입하면, 투입한 금액만큼 잔액이 증가한다.", () => {
+      expect(vendingMachine.getBalance()).toBe(0);
+
       const money = 10000;
       vendingMachine.addBalance(money);
       expect(vendingMachine.getBalance()).toBe(money);
     });
 
-    it("잘못된 금액(0 또는 음수)을 투입하면 오류가 발생한다", () => {
+    it("잘못된 금액(0 또는 음수)을 투입하면, 오류가 발생한다.", () => {
       expect(() => vendingMachine.addBalance(0)).toThrowError(
         InvalidAmountError,
       );
@@ -36,28 +38,51 @@ describe("VendingMachine", () => {
   });
 
   describe("resetBalance", () => {
-    it("금액을 반환하면 잔액이 0으로 초기화된다", () => {
+    it("금액을 반환하면, 잔액이 0으로 초기화된다.", () => {
+      expect(vendingMachine.getBalance()).toBe(0);
+
       vendingMachine.addBalance(1000);
+      expect(vendingMachine.getBalance()).toBe(1000);
+
       vendingMachine.resetBalance();
       expect(vendingMachine.getBalance()).toBe(0);
     });
 
-    it("잔액이 부족하면 금액을 반환할 수 없다", () => {
+    it("잔액이 0원이면, 예외가 발생한다.", () => {
       expect(() => vendingMachine.resetBalance()).toThrowError(
         NoBalanceToReturnError,
       );
     });
   });
 
-  describe("purchaseProduct", () => {
-    it("금액이 부족하면 제품을 구매할 수 없다", () => {
+  describe("purchaseProduct()", () => {
+    it("잔액이 제품의 금액보다 적으면, 제품을 구매할 수 없다.", () => {
+      const balance = products[1].price - 100;
+      vendingMachine.addBalance(balance);
+      expect(vendingMachine.getBalance()).toBe(balance);
+
       expect(() => vendingMachine.purchaseProduct(products[1])).toThrowError(
         InsufficientAmountError,
       );
     });
 
-    it("금액이 충분하면 제품을 구매할 수 있다", () => {
-      vendingMachine.addBalance(products[1].price);
+    it("잔액이 제품의 금액보다 많으면, 제품을 구매할 수 있다", () => {
+      const extraBalance = 100;
+      const balance = products[1].price + extraBalance;
+
+      vendingMachine.addBalance(balance);
+      expect(vendingMachine.getBalance()).toBe(balance);
+
+      vendingMachine.purchaseProduct(products[1]);
+      expect(vendingMachine.getBalance()).toBe(extraBalance); // 제품 가격만큼 차감됨
+    });
+
+    it("잔액이 제품의 금액과 같으면, 제품을 구매할 수 있다", () => {
+      const balance = products[1].price;
+
+      vendingMachine.addBalance(balance);
+      expect(vendingMachine.getBalance()).toBe(balance);
+
       vendingMachine.purchaseProduct(products[1]);
       expect(vendingMachine.getBalance()).toBe(0); // 제품 가격만큼 차감됨
     });
