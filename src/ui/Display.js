5dd8fe114963058e328cd9e341f42ad0b3cb3290
yeonhee7935 <yeonhee7935@naver.com>
2024-12-04 22:43:22 +0900

    [refactor] formatString을 별도의 함수로 추출(테스트 시 사용하기 위해)

--- src/ui/Display.js
+++ src/ui/Display.js
@@ -1,11 +1,10 @@
+import { formatString } from "../utils/format";
+
 export class Display {
   constructor(displayElement) {
     this.display = displayElement;
   }
   update(balance) {
-    this.display.textContent = this.#formatString(balance);
-  }
-  #formatString(str) {
-    return str.toLocaleString("ko-KR");
+    this.display.textContent = formatString(balance);
   }
 }
---
+++ src/utils/format.js
@@ -0,0 +1,3 @@
+export function formatString(str) {
+  return str.toLocaleString("ko-KR");
+}
