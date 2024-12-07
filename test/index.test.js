55aecf39de85ec7a3bc1e56b698ee4f049532300
yeonhee7935 <yeonhee7935@naver.com>
2024-12-07 14:41:35 +0900

    [test] msw로 서버 모킹(제품 조회 API)

--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,50 @@
-import { describe, it, expect, vi, beforeEach } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { listToDictionary, loadProducts } from "../src";
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
+
+const products = [
+  { id: "1", name: "콜라", price: 1500 },
+  { id: "2", name: "사이다", price: 1300 },
+];
+const productsWithOutId = [
+  { name: "콜라", price: 1500 },
+  { name: "사이다", price: 1300 },
+];
+
+const server = setupServer(
+  http.get("/products.json", () => {
+    return HttpResponse.json(products);
+  }),
+);
+
+describe("loadProducts()", () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it("정상적인 응답을 받을 경우 제품 목록을 반환한다.", async () => {
+    const result = await loadProducts();
+    expect(result).toEqual(products);
+  });
+
+  it("서버 오류가 발생한 경우, 빈 배열을 반환한다.", async () => {
+    // 서버 오류(500) 시뮬레이션
+    server.use(
+      http.get("/products.json", (req, res) => {
+        return HttpResponse.error();
+      }),
+    );
+
+    const result = await loadProducts();
+    expect(result).toEqual([]); // 실패 시 빈 배열을 반환
+  });
+});
 
 describe("listToDictionary()", () => {
-  const products = [
-    { id: "1", name: "콜라", price: 1500 },
-    { id: "2", name: "사이다", price: 1300 },
-  ];
-  const productsWithOutId = [
-    { name: "콜라", price: 1500 },
-    { name: "사이다", price: 1300 },
-  ];
-
   it("리스트의 각 요소가 id 필드를 갖는 경우, 리스트를 딕셔너리로 변환한다.", () => {
     const dictionary = listToDictionary(products);
 
